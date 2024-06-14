4c45fb050ad2648fd1581c9d1a17114e11cbefc2
simonhulme <135348115+simonhulme@users.noreply.github.com>
2024-06-14 09:37:55 +0100

    initial commit

---
+++ 04_data_pre_processing.R
@@ -0,0 +1,22 @@
+# Data Pre Processing ----
+
+## Set Up ----
+
+# Load Libraries
+
+library(tidyverse)
+library(timetk)
+library(DataExplorer)
+
+# Import Data 
+
+## Start with Totals 
+
+total_appointments   <-
+    read_rds("00_data/processed/sliced/total/wakefield_total_5_day_tbl.rds")
+
+appointments_by_mode <-
+    read_rds("00_data/processed/sliced/appt_mode/wakefield_appt_mode_5_day_sliced_tbl.rds")
+
+appointments_by_hcp <- 
+    read_rds("00_data/processed/sliced/hcp_type/wakefield_hcp_type_5_day_sliced_tbl.rds")

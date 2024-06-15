8d53d4a5ed7ed8c7aafc893d5a7a1cbd543fa529
simonhulme <135348115+simonhulme@users.noreply.github.com>
2024-06-15 09:58:34 +0100

    access data using FingertipsR package

---
+++ create_explanatory_feature_time_series.R
@@ -0,0 +1,36 @@
+# create time series for potential explanatory features ----
+
+## Extract data for a single GP practice
+
+## Trinity Medical Centre ----
+
+### Codes ----
+
+## WFD ODS CODE : 03R
+## WFD PHE CODE : E38000190
+## PRACTICE CODE: B87017
+
+library(tidyverse)
+library(fingertipsR)
+
+inds <- indicators(ProfileID = 21)
+
+### QOF Population Data ----
+
+qof_annual_population <- 
+    fingertips_data(IndicatorID = 114, AreaTypeID = 7) %>% 
+    as_tibble() %>% 
+    filter(AreaCode == "B87017") %>% 
+    select(Timeperiod, Value) %>% 
+    mutate(date = str_split_i(Timeperiod, "/", 1)) %>% 
+    mutate(date = as.Date(paste0(date, "-04-01"))) %>% 
+    mutate(date = date + years(1)) %>% 
+    select(date, population = Value)
+
+write_rds(qof_annual_population, "00_data/raw/qof_annual_population.rds")
+
+### Registered Population Data ----
+
+registered_annual_population <-  
+    fingertips_data(IndicatorID = 337, AreaTypeID = 7)
+

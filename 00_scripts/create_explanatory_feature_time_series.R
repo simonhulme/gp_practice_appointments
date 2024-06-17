453f909d88b1ce5728c8d4c15636015e857b2074
simonhulme <135348115+simonhulme@users.noreply.github.com>
2024-06-17 19:34:13 +0100

    population data

--- 00_scripts/create_explanatory_feature_time_series.R
+++ 00_scripts/create_explanatory_feature_time_series.R
@@ -1,9 +1,76 @@
-# create time series for potential explanatory features ----
-
-# Extract Monthly GP Online Consultation Data for chosen area (Wakefield) ----
+# Create time series for potential explanatory features ----
 
 library(tidyverse)
+library(rvest)
 library(timetk)
+
+# Population
+# On-line Consultations
+# Telephone Consultation
+
+## Population of chosen area ----
+
+## Available monthly with files accessed from different web pages 
+## Web pages have same base url but each ends with different month and year
+
+## Construct vector of urls for target dates 
+
+base_url <- 
+    "https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice/"
+
+### Create sequence of dates and transform into web address format
+
+convert_date <- function(date) {
+    
+    year   <- year(date)
+    month  <- str_to_lower(month(date, label = TRUE, abbr = FALSE))
+    
+    output <- paste(month, year, sep = "-")
+    
+    return(output)
+}
+
+dates <- seq.Date(from = as.Date("2019-05-01"), to = as.Date("2024-05-01"), by = "month")
+
+append_dates_url <- map_chr(dates, convert_date)
+
+### Create vector of full url (base + date)
+
+full_url <- paste0(base_url, append_dates_url)
+
+## Extract address for data from each webpage 
+
+get_urls_for_registered_populations <- function(url) {
+    
+    html_obj <- read_html(url)
+    
+    data_url <- 
+        html_elements(html_obj, "body") %>% 
+        html_element("#resources > div:nth-child(2) > div:nth-child(1) > div > a") %>% 
+        html_attr("href")
+    
+    return(data_url)
+}
+
+urls <- map(full_url, get_urls_for_registered_populations)
+
+csv_urls <- urls[str_detect(urls, "csv")]
+
+## download data using urls 
+
+registered_population <- 
+    map(csv_urls, read_csv) %>% 
+    map(~ .x %>% mutate(EXTRACT_DATE = dmy(EXTRACT_DATE))) %>% 
+    reduce(bind_rows) %>%
+    janitor::clean_names() %>% 
+    filter(sub_icb_location_code == "03R" | ccg_code == "03R") %>%
+    summarise_by_time(.date_var = extract_date, .by = "month", registered_population = sum(number_of_patients))
+
+## save data
+write_rds(registered_population, "00_data/processed/wakefield_population_monthly.rds")
+
+
+## Extract Monthly GP Online Consultation Data for chosen area (Wakefield) ----
 
 ## SUB-ICB LOCATION CODE: 03R
 
@@ -47,7 +114,7 @@ df <-
 
 df
 
-## Patients Registered ----
+# Patients Registered by Area----
 
 df <- get_gp_data_daily(url = "https://files.digital.nhs.uk/C1/9B13E8/gp-reg-pat-prac-all.zip") %>% pluck(1)
 
@@ -73,7 +140,14 @@ urls_zip_no_date <-
 
 urls_csv <- c(
       "https://files.digital.nhs.uk/19/AADF93/gp-reg-pat-prac-all.csv",
-      "https://files.digital.nhs.uk/AF/9B0FC8/gp-reg-pat-prac-all.csv")
+      "https://files.digital.nhs.uk/AF/9B0FC8/gp-reg-pat-prac-all.csv",
+      "https://files.digital.nhs.uk/F1/3E4D66/gp-reg-pat-prac-all.csv",
+      "https://files.digital.nhs.uk/87/E03B72/gp-reg-pat-prac-all.csv",
+      "https://files.digital.nhs.uk/64/D830C6/gp-reg-pat-prac-all.csv",
+      "https://files.digital.nhs.uk/7C/D10294/gp-reg-pat-prac-all.csv",
+      "https://files.digital.nhs.uk/ED/C675EF/gp-reg-pat-prac-all.csv",
+      "https://files.digital.nhs.uk/D7/73169E/gp-reg-pat-prac-all.csv"
+      )
 
 data_1 <- 
     map(url_zip_date, ~ get_gp_data_daily(.x) %>% pluck(1))
@@ -86,6 +160,12 @@ data_3 <-
     map(urls_csv, read_csv) %>% 
     map(~ .x %>% mutate(EXTRACT_DATE = dmy(EXTRACT_DATE)))
 
-df <- reduce(list(data_1, data_2, data_3), bind_rows)
+df <- 
+    reduce(list(data_1, data_2, data_3), bind_rows) %>%
+    janitor::clean_names() %>% 
+    filter(sub_icb_location_code == "03R") %>%
+    summarise_by_time(.date_var = extract_date, .by = "day", registered_population = sum(number_of_patients))
 
-df %>% arrange(EXTRACT_DATE)
+df %>%
+    plot_time_series(.date_var = extract_date, registered_population, .smooth= FALSE)
+ﬂ

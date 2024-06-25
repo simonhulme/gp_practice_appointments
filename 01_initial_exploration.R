5ab29f05ecbb6775ec7afacc01e3dffb1988da4f
simonhulme <135348115+simonhulme@users.noreply.github.com>
2024-06-25 12:02:02 +0100

    re exploration using refined data

--- 01_initial_exploration.R
+++ 01_initial_exploration.R
@@ -1,7 +1,5 @@
 # Initial Data Exploration ----
 
-## Set Up ----
-
 # Load Libraries
 
 library(tidyverse)
@@ -24,8 +22,10 @@ total_daily_appts <-
     )
 
 total_daily_appts %>%
-    plot_time_series(.date_var = appointment_date,
-                     .value = appointments) 
+    plot_time_series(
+        .date_var = appointment_date,
+        .value = appointments, 
+        .title = "Weekly low values due to weekends") 
 
 total_weekly_appts <- 
     wakefield_daily_raw %>% 
@@ -36,9 +36,10 @@ total_weekly_appts <-
     )
 
 total_weekly_appts %>%
-    plot_time_series(.date_var = appointment_date,
-                     .value = appointments)
-
+    plot_time_series(
+        .date_var = appointment_date,
+        .value = appointments,
+        .title = "Annual low values due to Xmas and New Year Holidays")
 
 total_monthly_appts <- 
     wakefield_daily_raw %>% 
@@ -49,25 +50,12 @@ total_monthly_appts <-
     )
 
 total_monthly_appts %>%
-    plot_time_series(.date_var = appointment_date,
-                     .value = appointments)
+    plot_time_series(
+        .date_var = appointment_date,   
+        .value = appointments,
+        .title = "Low total appointments due to pandemic but approx linear increase since then")
 
-### By Feature ----
-
-#### 1. Appointment Mode ----
-
-appt_mode_daily_appts <- 
-    wakefield_daily_raw %>%
-    group_by(appt_mode) %>%
-    summarise_by_time(
-        .date_var = appointment_date,
-        .by = "day",
-        appointments = sum(count_of_appointments)
-    ) %>% 
-    ungroup()
-
-appt_mode_daily_appts %>% 
-    plot_time_series(.date_var = appointment_date, .value = appointments, .facet_vars = appt_mode)
+### Appointment Mode ----
 
 appt_mode_weekly_appts <- 
     wakefield_daily_raw %>%
@@ -79,23 +67,25 @@ appt_mode_weekly_appts <-
     ) %>% 
     ungroup()
 
-appt_mode_weekly_appts %>% 
-    plot_time_series(.date_var = appointment_date, .value = appointments, .facet_vars = appt_mode)
-
-#### 2. Appointment Status ----
-
-appt_status_daily_appts <- 
-    wakefield_daily_raw %>%
-    group_by(appt_status) %>%
-    summarise_by_time(
+appt_mode_weekly_appts %>%
+    plot_time_series(
         .date_var = appointment_date,
-        .by = "day",
-        appointments = sum(count_of_appointments)
-    ) %>% 
-    ungroup()
+        .value = appointments,
+        .facet_vars = appt_mode,
+        .title = "High frequency 'Unknown' at start of time series"
+    )
 
-appt_status_daily_appts %>% 
-    plot_time_series(.date_var = appointment_date, .value = appointments, .facet_vars = appt_status)
+appt_mode_weekly_appts %>%
+    filter_by_time(.date_var = appointment_date,
+                   .start_date = "2019-03",
+                   .end_date = "2024") %>%
+    filter(appt_mode == "Unknown") %>%
+    plot_time_series(
+        .date_var = appointment_date,
+        .value = appointments,
+        .title = "Start date of 2019-03 handles initial 'Unknown' values ")
+
+### Appointment Status ----
 
 appt_status_weekly_appts <- 
     wakefield_daily_raw %>%
@@ -107,24 +97,25 @@ appt_status_weekly_appts <-
     ) %>% 
     ungroup()
 
-appt_status_weekly_appts %>% 
-    plot_time_series(.date_var = appointment_date, .value = appointments, .facet_vars = appt_status)
-
-
-#### 3. Health Care Professional ----
-
-hcp_type_daily_appts <- 
-    wakefield_daily_raw %>%
-    group_by(hcp_type) %>%
-    summarise_by_time(
+appt_status_weekly_appts %>%
+    plot_time_series(
         .date_var = appointment_date,
-        .by = "day",
-        appointments = sum(count_of_appointments)
-    ) %>% 
-    ungroup()
+        .value = appointments,
+        .facet_vars = appt_status,
+        .title = "High frequency 'Not Provided' at start of time series"
+    )
 
-hcp_type_daily_appts %>% 
-    plot_time_series(.date_var = appointment_date, .value = appointments, .facet_vars = hcp_type)
+appt_status_weekly_appts %>% 
+    filter_by_time(.date_var = appointment_date,
+                   .start_date = "2019-03",
+                   .end_date = "2024") %>%
+    plot_time_series(
+        .date_var = appointment_date,
+        .value = appointments,
+        .facet_vars = appt_status,
+        .title = "Start date of 2019-03 handles initial 'Not Provdided' values ")
+    
+### Health Care Professional ----
 
 hcp_type_weekly_appts <- 
     wakefield_daily_raw %>%
@@ -136,44 +127,27 @@ hcp_type_weekly_appts <-
     ) %>% 
     ungroup()
 
-hcp_type_weekly_appts %>% 
-    plot_time_series(.date_var = appointment_date, .value = appointments, .facet_vars = hcp_type)
+hcp_type_weekly_appts %>%
+    plot_time_series(
+        .date_var = appointment_date,
+        .value = appointments,
+        .facet_vars = hcp_type,
+        .title = "High frequency 'Not Provided' or 'Unknown' at start of time series"
+    )
 
-wakefield_daily_raw %>% names()
+hcp_type_weekly_appts %>% 
+    filter_by_time(.date_var = appointment_date, .start_date = "2019-03", .end_date = "2024") %>% 
+    plot_time_series(
+        .date_var = appointment_date,
+        .value = appointments,
+        .facet_vars = hcp_type,
+        .title = "Start date of 2019-03 handles initial 'Not Provided' and 'Unknown' values"
+    )
+
 
 #### 4. Time between booking and appointment ----
 
-time_between_book_and_appt_daily_appts <-
-    wakefield_daily_raw %>%
-    mutate(time_between_book_and_appt =
-               factor(
-                   time_between_book_and_appt,
-                   levels = c(
-                       "Same Day",
-                       "1 Day",
-                       "2 to 7 Days",
-                       "8  to 14 Days",
-                       "15  to 21 Days",
-                       "22  to 28 Days",
-                       "More than 28 Days",
-                       "Unknown / Data Issue",
-                       "Unknown / Data Quality"
-                   )
-               )) %>%
-    group_by(time_between_book_and_appt) %>%
-    summarise_by_time(
-        .date_var = appointment_date,
-        .by = "day",
-        appointments = sum(count_of_appointments)
-    ) %>%
-    ungroup()
-
-time_between_book_and_appt_daily_appts %>%
-    plot_time_series(.date_var = appointment_date,
-                     .value = appointments,
-                     .facet_vars = time_between_book_and_appt, .facet_ncol = 2)
-
-time_between_book_and_appt_weekly_appts <-
+book_appt_time_weekly_appts <-
     wakefield_daily_raw %>%
     mutate(time_between_book_and_appt =
                factor(
@@ -198,51 +172,23 @@ time_between_book_and_appt_weekly_appts <-
     ) %>%
     ungroup()
 
-time_between_book_and_appt_weekly_appts %>%
-    plot_time_series(.date_var = appointment_date,
-                     .value = appointments,
-                     .facet_vars = time_between_book_and_appt, .facet_ncol = 2)
-
-time_between_book_and_appt_monthly_appts <-
-    wakefield_daily_raw %>%
-    mutate(time_between_book_and_appt =
-               factor(
-                   time_between_book_and_appt,
-                   levels = c(
-                       "Same Day",
-                       "1 Day",
-                       "2 to 7 Days",
-                       "8  to 14 Days",
-                       "15  to 21 Days",
-                       "22  to 28 Days",
-                       "More than 28 Days",
-                       "Unknown / Data Issue",
-                       "Unknown / Data Quality"
-                   )
-               )) %>%
-    group_by(time_between_book_and_appt) %>%
-    summarise_by_time(
+book_appt_time_weekly_appts %>%
+    plot_time_series(
         .date_var = appointment_date,
-        .by = "month",
-        appointments = sum(count_of_appointments)
-    ) %>%
-    ungroup()
-
-time_between_book_and_appt_monthly_appts %>%
-    plot_time_series(.date_var = appointment_date,
-                     .value = appointments,
-                     .facet_vars = time_between_book_and_appt, .facet_ncol = 2)
+        .value = appointments,
+        .facet_vars = time_between_book_and_appt,
+        .facet_ncol = 2,
+        .title = "Groups for Unknown and Data Issue / Quality"
+    )
 
 # FINDINGS ----
 
-## 1. data assumes 7 day week but GP opening times relate to 5 day business week ----
-
-## 2. historic data quality issues near start of time series ----
-### * different time periods depending on feature ----
-
-## 3. categorical data encoded as strings ----
-
-## 4. levels duplicated ----
+## 1. data assumes 7 day week rather than 5 day working week ----
+## 2. bank holidays result in low values ----
+## 3. pandemic had significant impact on total appointments ----
+## 4. historic data quality issues near start of time series ----
+## 5. categorical data encoded as strings ----
+## 6. levels duplicated ----
 ### * video / online appointment modes ----
 ### * unknown vs. data not provided ----
 ### • data quality vs. data issue
@@ -253,4 +199,4 @@ time_between_book_and_appt_monthly_appts %>%
 ## * convert to 5-day business week ----
 ## * encode categorical variables as factors ----
 ## * combine levels for features where duplicated ----
-## * slice time series to exclude significant data quality problems ----
+## * create subset of time series to exclude significant data quality problems ----

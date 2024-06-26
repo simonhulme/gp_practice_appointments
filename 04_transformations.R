27115890c28c1295dd7cc27a10e8827ea68fc1c2
simonhulme <135348115+simonhulme@users.noreply.github.com>
2024-06-26 15:32:47 +0100

    added training half days

--- 04_transformations.R
+++ 04_transformations.R
@@ -1,19 +1,78 @@
-# Transformations for improved models ----
-
-
-
+# Transformations ----
 
 library(tidyverse)
 library(timetk)
 
-all_appointments_daily_tbl <- read_rds("00_data/processed/wakefield_working_week_daily.rds")
+wakefield_working_week_daily <- read_rds("00_data/processed/wakefield_working_week_daily.rds")
 
-## Transformations and adjustments ----
+## Calendar Adjustments ----
 
-### Calendar Adjustments ----
+## Holidays, Pandemic, Training Days
+## Create Flags and adjust weekly values by using mean per day / session
 
-# use mean daily appointments weekly / monthly
-# to reflect workload around Bank Holidays
+## apply to whole time series prior to creating more focused analysis sets 
+
+### Start with Holidays
+
+### Add Holiday Sequence ----
+
+start_date <- min(wakefield_working_week_daily$appointment_date)
+end_date   <- max(wakefield_working_week_daily$appointment_date)
+
+
+holiday_sequence <-
+    tk_make_holiday_sequence(start_date = start_date,
+                             end_date = end_date,
+                             calendar = "LONDON")
+
+wakefield_working_week_with_holidays_daily <- 
+    wakefield_working_week_daily %>%
+    mutate(holiday = as_factor(if_else(appointment_date %in% holiday_sequence, "Yes", "No")))
+
+### Add Training Afternoons ----
+
+training_dates_19 <- 
+    c("2019-03-20", "2019-04-24", "2019-05-15", "2019-06-19",
+      "2019-07-17", "2019-09-18", "2019-10-16", "2019-11-13")
+
+training_dates_20 <- 
+    c("2020-01-15", "2020-02-26", "2020-07-15", "2020-08-19", "2020-09-16", 
+      "2020-10-14", "2020-11-11")
+
+training_dates_21 <- 
+    c("2021-01-13", "2021-02-24", "2021-03-17", "2021-04-21", "2021-05-12",
+      "2021-06-16", "2021-07-14", "2021-09-15", "2021-10-13", "2021-11-10")
+
+training_dates_22 <- 
+    c("2022-02-23", "2022-03-16", "2022-04-06", "2022-05-11","2022-06-15", 
+      "2022-07-13", "2022-09-14", "2022-10-12", "2022-11-09")
+
+training_dates_23 <- 
+    c("2023-02-08", "2023-04-19", "2023-05-17", "2023-06-21", "2023-07-12", 
+      "2023-08-16", "2023-09-13", "2023-10-11", "2023-11-15")
+
+training_dates_24 <- 
+    c("2024-01-17", "2024-02-21", "2024-03-13", "2024-04-17", "2024-05-15",
+      "2024-06-19", "2024-07-10", "2024-09-18", "2024-10-16", "2024-11-13")
+
+training_dates <-
+    as.Date(c(
+        training_dates_19,
+        training_dates_20,
+        training_dates_21,
+        training_dates_22,
+        training_dates_23,
+        training_dates_24
+    ))
+
+wakefield_working_week_with_holidys_and_training_daily <- 
+    wakefield_working_week_with_holidays_daily %>% 
+    mutate(training = if_else(appointment_date %in% training_dates, "Yes", "No"))
+
+wakefield_working_week_with_holidys_and_training_daily
+
+# PROGRESS UP TO HERE ----
+
 
 all_appointments_daily_tbl %>%
     # filter(hcp_type == "GP", appt_mode == "Face-to-Face", appt_status == "Attended") %>%
@@ -159,31 +218,7 @@ broom::augment(glm_boxcox_fitted, gp_appointments_weekly) %>%
     theme_bw()
 
 
-# Add Holiday Sequence ----
 
-holidays <-
-    tk_make_holiday_sequence(start_date = "2019-03-01",
-                             end_date = "2024-04-30",
-                             calendar = "LONDON")
-
-daily_appts_with_holidays <- 
-    all_appointments_daily_tbl %>%
-    summarise_by_time(
-        .date_var = appointment_date,
-        .by = "day",
-        appointments = sum(count_of_appointments)
-    ) %>%
-    mutate(holiday = as_factor(if_else(appointment_date %in% holidays, "Yes", "No")))
-
-daily_appts_with_holidays %>%
-    plot_time_series_regression(
-        .date_var = appointment_date,
-        .formula = appointments ~ 
-            day(appointment_date) +
-            week(appointment_date) +
-            month(appointment_date, label = TRUE): holiday +
-            year(appointment_date),
-        .show_summary = TRUE)
 
 ## may be better manually modelling this using glm and poisson regression
 
